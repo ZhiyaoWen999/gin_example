3fd3050df0bff6fc1d25a1fdbf5e891347d06351
Zhiyao <15222848787@163.com>
2023-02-03 17:05:30 -0800

    📝 Add JWT token

--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"gin_example/middleware/jwt"
 	"gin_example/pkg/setting"
+	"gin_example/routers/api"
 	v1 "gin_example/routers/api/v1"
 
 	"github.com/gin-gonic/gin"
@@ -15,12 +17,14 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 
 	gin.SetMode(setting.RunMode)
+	r.GET("/auth", api.GetAuth)
 
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)
-		//新建标签
+		//新建标签g
 		apiv1.POST("/tags", v1.AddTag)
 		//更新指定标签
 		apiv1.PUT("/tags/:id", v1.EditTag)
